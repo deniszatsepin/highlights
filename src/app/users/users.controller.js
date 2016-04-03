22db095814d31c4b6c0a00ec82551dc4eac720d2
Denis Zatsepin <denis@zatsepin.spb.ru>
2016-04-03 23:19:41 +0200

    Add user controller

---
+++ src/app/users/users.controller.js
@@ -0,0 +1,42 @@
+import {Sequelize} from '../data/models';
+
+export default class UsersController {
+
+	create(params) {
+
+	}
+
+	update(id) {
+
+	}
+
+	find(params) {
+
+	}
+
+	get(id) {
+
+	}
+
+	remove(id) {
+
+	}
+
+	validatEmail() {
+
+	}
+
+	/**
+	 *	Reset user password using token from email
+	 */
+	resetPassword(token) {
+
+	}
+
+	/**
+	*	Request reset of the user password
+	*/
+	requestReset(email) {
+
+	}
+}
